a754ae620d74b8b228c07e27b36ba05522e85d3d
K.ze <110768373+kosarz-endehboodi@users.noreply.github.com>
2023-12-09 15:15:15 +0330

    Create Salary.py

---
+++ Salary.py
@@ -0,0 +1,12 @@
+def calculating(salary):
+     insurance=salary*0.07
+     b=salary*0.10
+     income=salary*0.83
+
+
+      print (insurance, b,salary)
+
+
+  salary=float(input("inter salary"))
+
+     calculating(salary)
